56f31968a317d1818857547ae12e8dc7760899db
Mykhailo <115115470+MykhailoMaidiuk@users.noreply.github.com>
2024-05-22 22:37:26 +0200

    Delete backend directory

--- backend/src/main/java/com/sp/weather/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sp.weather.repository;
-import com.sp.weather.entity.Users;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface UserRepository  extends CrudRepository<Users, String> {
-}
